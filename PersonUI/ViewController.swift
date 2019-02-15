b9adcaea6013f7d53300e11a074eb97fdd116565
rjy7wb <rjy7wb@github.com>
2019-02-15 15:06:35 -0600

    INIT AND FINIT

---
+++ PersonUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonUI
+//
+//  Created by rjy7wb on 2/15/19.
+//  Copyright © 2019 rjy7wb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
